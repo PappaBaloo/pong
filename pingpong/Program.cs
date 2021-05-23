ffc773f7f584c15b1c101f1c82e3bca8cdd24178
Charlie Sesser Nylund <charlie.sessernylund@elev.ga.ntig.se>
2021-05-23 20:57:03 +0200

    initial commit

---
+++ pingpong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pongping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
